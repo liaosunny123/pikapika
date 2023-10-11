f13f5c62838bbc9483bc7407202ba760fc33bcce
EpicMo <1982742309@qq.com>
2023-10-11 23:36:14 +0800

    feat: 加强对于后端错误的鲁棒性

--- callback/pic_gen.py
+++ callback/pic_gen.py
@@ -39,22 +39,24 @@ def callback(ch, method, properties, body):
             else:
                 ch.basic_ack(delivery_tag=method.delivery_tag)
         except Exception as e:
-            trace = uuid.uuid4()
-            logger.error(
-                f"Pikapika meets a trouble when dealing pic-gen, error: {e}, trace-id: {trace}"
-            )
-            response = requests.post(
-                data["callback"],
-                data={
-                    "ret": "403",
-                    "msg": f"服务器遇到内部错误，请联系管理员查看，Trace Id 为：{trace}",
-                    "pictures": [],
-                },
-            )
-            if response.status_code != 200:
+            if "callback" in data and data["callback"][:3] == "http":
+                trace = uuid.uuid4()
                 logger.error(
-                    f'Http request meet trouble, can not connect with remote server: {data["callback"]}'
+                    f"Pikapika meets a trouble when dealing pic-gen, error: {e}, trace-id: {trace}"
                 )
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-            sleep(10 * i * 1000)
-            i = i + 1
+                response = requests.post(
+                    data["callback"],
+                    data={
+                        "ret": "403",
+                        "msg": f"服务器遇到内部错误，请联系管理员查看，Trace Id 为：{trace}",
+                        "pictures": [],
+                    },
+                )
+                if response.status_code != 200:
+                    logger.error(
+                        f'Http request meet trouble, can not connect with remote server: {data["callback"]}'
+                    )
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+                sleep(10 * i * 1000)
+                i = i + 1
+            logger.error(f'Meet error content for {data}')
